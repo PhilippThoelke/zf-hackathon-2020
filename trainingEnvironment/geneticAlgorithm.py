f60bb901501d5852c4918095e47a7829f47ba907
PhilippThoelke <p.thoelke@gmx.de>
2020-01-11 17:46:13 +0100

    Switch to pytorch

--- trainingEnvironment/geneticAlgorithm.py
+++ trainingEnvironment/geneticAlgorithm.py
@@ -1,15 +1,29 @@
 from environment import Simulator
 from dataProcessing import ProfileManager
-from tensorflow.keras import models, layers
 import numpy as np
 from matplotlib import pyplot as plt
-from threading import Thread
+import torch
+from torch import nn
+from joblib import Parallel, delayed
+
+class ANN(nn.Module):
+    def __init__(self):
+        super(ANN, self).__init__()
+        self.layers = [
+            nn.Linear(9, 8), torch.relu,
+            nn.Linear(8, 1), torch.sigmoid
+        ]
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
 
 class GeneticAlgorithm:
 
-    POPULATION_SIZE = 20
-    NUM_SURVIVORS = 10
-    MUTATION_RATE = 0.025
+    POPULATION_SIZE = 32
+    NUM_SURVIVORS = 16
+    MUTATION_RATE = 0.05
     MUTATION_SCALE = 1
     EVALUATION_STEPS = 750
 
@@ -22,9 +36,7 @@ class GeneticAlgorithm:
         self.road_profile = ProfileManager()
 
     def evaluate(self):
-        fitness = []
-        for model in self.population:
-            fitness.append(self._simulate(model))
+        fitness = Parallel(n_jobs=8)(delayed(GeneticAlgorithm._simulate)(model, self.road_profile.training_profile[0]) for model in self.population)
         return np.array(fitness)
 
     def optimization_step(self):
@@ -38,53 +50,61 @@ class GeneticAlgorithm:
         for i in range(GeneticAlgorithm.NUM_SURVIVORS, len(self.population)):
             # randomly choose two parents from the surviving population
             parent1, parent2 = np.random.choice(self.population[:GeneticAlgorithm.NUM_SURVIVORS], size=2, replace=False)
-            weights1 = parent1.get_weights()
-            weights2 = parent2.get_weights()
+            weights1 = GeneticAlgorithm._get_weights(parent1)
+            weights2 = GeneticAlgorithm._get_weights(parent2)
 
             # generate new model weights using crossover and mutation
             new_weights = GeneticAlgorithm._crossover(weights1, weights2)
-            GeneticAlgorithm._mutate(new_weights)
+            new_weights = GeneticAlgorithm._mutate(new_weights)
 
             # insert the new model into the population
-            self.population[i] = GeneticAlgorithm._get_model(weights=new_weights)
+            GeneticAlgorithm._set_weights(self.population[i], new_weights)
 
-    def _simulate(self, model):
-        env = Simulator(self.road_profile.training_profile[0])
+    def _simulate(model, road_profile):
+        env = Simulator(road_profile)
         x = env.states[-1]
         for step in range(GeneticAlgorithm.EVALUATION_STEPS):
-            x = env.next(model(x.reshape((1,) + x.shape)).numpy()[0,0])
+            x_torch = torch.from_numpy(x.reshape((1,) + x.shape))
+            x = env.next(model(x_torch)[0,0] * 2)
         return env.score()
 
     def _crossover(weights1, weights2):
         new_weights = []
         for var1, var2 in zip(weights1, weights2):
             assert var1.shape == var2.shape
-            mask = np.random.uniform(low=0, high=1, size=var1.shape) < 0.5
-            new_var = np.empty(var1.shape)
+            mask = torch.rand(size=var1.shape) < 0.5
+            new_var = torch.empty(var1.shape)
             new_var[mask] = var1[mask]
             new_var[~mask] = var2[~mask]
             new_weights.append(new_var)
         return new_weights
 
     def _mutate(weights):
+        new_weights = []
         for var in weights:
-            mask = np.random.uniform(low=0, high=1, size=var.shape) < GeneticAlgorithm.MUTATION_RATE
-            old = np.array(var)
-            var[mask] += np.random.normal(loc=0, scale=GeneticAlgorithm.MUTATION_SCALE, size=var.shape)[mask]
+            mask = torch.rand(size=var.shape) < GeneticAlgorithm.MUTATION_RATE
+            new_var = var.clone()
+            new_var[mask] += torch.randn(size=var.shape)[mask] * GeneticAlgorithm.MUTATION_SCALE
+            new_weights.append(new_var)
+        return new_weights
 
-    def _get_model(weights=None):
-        model = models.Sequential()
-        model.add(layers.Input(shape=(9,)))
-        model.add(layers.Dense(units=8, activation='relu'))
-        model.add(layers.Dense(units=1, activation='sigmoid'))
-
-        if weights is not None:
-            model.set_weights(weights)
+    def _get_model():
+        model = ANN()
         return model
+
+    def _get_weights(model):
+        return [layer.weight for layer in model.layers if hasattr(layer, 'weight')]
+
+    def _set_weights(model, weights):
+        index = 0
+        for layer in model.layers:
+            if hasattr(layer, 'weight'):
+                layer.weight = nn.Parameter(weights[index])
+                index += 1
 
 if __name__ == '__main__':
     ga = GeneticAlgorithm()
-    for step in range(25):
+    for step in range(100):
         if step > 0:
             print(f'Optimization step {step} (fitness: {ga.history[-1]})')
         ga.optimization_step()
