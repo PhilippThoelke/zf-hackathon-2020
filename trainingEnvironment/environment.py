40b8583ec11bde8f1705f2e84f5149e9bebefef2
Charlie Lange <charlie.lange@posteo.de>
2020-01-11 10:44:18 +0100

    extended simulator

--- trainingEnvironment/environment.py
+++ trainingEnvironment/environment.py
@@ -1,15 +1,23 @@
 import numpy as np
 
 class Simulator:
-    def __init(self)__:
-        pass
+    def __init__(self, n):
+        self.states.append(self.reset())
+        self.n = n
 
     def next(self, current):
         # perform one solving step of the differential equation using the given current i
-
+        current state = np.zeros(9)
+        self.states.append(current_state)
         # return the current state
-        return np.zeros(6)
+        return current_state
 
     def reset(self):
         # return initial state
+        self.states = []
         return np.zeros(6)
+
+    def passs_on(self):
+        # poss on list of last last n states
+        return self.states[-self.n:]
+
