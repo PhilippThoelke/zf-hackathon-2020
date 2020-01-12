4bbb231b171e39b940debf8cf46038ee0851471a
PhilippThoelke <p.thoelke@gmx.de>
2020-01-12 18:45:58 +0100

    Make it parallel

--- trainingEnvironment/datasetPickler.py
+++ trainingEnvironment/datasetPickler.py
@@ -1,10 +1,13 @@
 import pickle
 from dataProcessing import ProfileManager
+from joblib import Parallel, delayed
 
 FILE = 'ts4_k_20.0.csv'
-VELOCITIES = [8, 20, 27]
+VELOCITIES = [8.3, 13.8, 19.4, 27.7]
 
-for vel in VELOCITIES:
+def run(vel):
     road_profile = ProfileManager.csv_to_profile(FILE, [vel])[0]
-    with open(FILE + f'_VEL_{vel}.pickle', 'wb') as file:
-        pickle.dump(road_profile)
+    with open('pickleData/' + FILE + f'_vel{vel}.pickle', 'wb') as file:
+        pickle.dump(road_profile, file)
+
+Parallel(n_jobs=2)(delayed(run)(vel) for vel in VELOCITIES)
