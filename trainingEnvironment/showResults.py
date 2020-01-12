f1bae341fe5b309abe5c425e8eee7b0c4e058a89
PhilippThoelke <p.thoelke@gmx.de>
2020-01-12 18:54:09 +0100

    Change input file

--- trainingEnvironment/showResults.py
+++ trainingEnvironment/showResults.py
@@ -6,10 +6,12 @@ from environment import Simulator
 from scipy import signal
 from evaluator import t_target, constraint_satisfied
 
+FILE = 'result_i_0.csv'
+
 PLOT_OFFSET = 300
 PLOT_LENGTH = 3000
 
-df = pd.read_csv('result_i_0.csv')
+df = pd.read_csv(FILE)
 
 print(df)
 
